c9a106b042809b1117bc65b51628c8b1148a2e26
atellezf <alejandro.tellez@gmail.com>
2021-11-23 19:33:47 -0600

    Se comenta el atributo edad de la clase Solicitante

--- src/com/company/finance/SolicitanteBuilder.java
+++ src/com/company/finance/SolicitanteBuilder.java
@@ -7,7 +7,7 @@ public class SolicitanteBuilder {
     // Ocupo un método estático para crear un objeto de Solicitante, debido a que es un proceso
     // complejo la creación de dicho objeto. Así simplificamos la creación de Solicitantes en la clase
     // principal.
-    // En la práctica profesional este método y los de capturar deberían estar en una clase externa conocida como
+    // En la práctica profesional este método y los de capturar deben estar en una clase externa conocida como
     // Façade, la cual es un patrón de diseño que facilita la creación de objetos complejos.
     // Revisar tutorial: https://www.tutorialspoint.com/design_pattern/facade_pattern.htm
     public static Solicitante crearSolicitante() {
